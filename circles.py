a2d8ba8997ab72a7a6e180e330e0045c57f810bd
Stephen Markacs <stephen.markacs@gmail.com>
2022-01-23 15:12:37 -0500

    wip

--- circles.py
+++ circles.py
@@ -6,7 +6,7 @@ from PIL import Image, ImageDraw, ImageFont
 
 # timing and scaling
 NUM_STEPS = 400
-MAX_SCALE = 10
+MAX_SCALE = 6
 
 # canvas 
 W = 800
@@ -16,13 +16,32 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 
+# CIRCLES FRACTAL MATH
+# factor is ratio of the full circle radius to that of the radius of the next larger circles 3
+FACTOR = 1 + 0.5 * ( math.sqrt(3) + ( 1 / math.sqrt(3) ) )
+
+
 font = ImageFont.load_default()
 
-def circles(image_draw, scale):
-    image_draw.ellipse(
-        (scale * 10, scale * 10, scale * 100, scale * 100), 
-        fill=WHITE, outline=RED
-    )    
+def circles(image_draw, remain, 
+            x, y, r):
+    # i find it feels cleaner to let the children do nothing here 
+    # than to have to worry about termination when i recurse
+    # may change my mind, thinking...
+    if remain > 0:
+        # funny toggle?
+        color = WHITE if remain % 2 else BLACK
+
+        # primary circle - can this be the only circle in this iteration?
+        image_draw.ellipse(
+            (x - r, y - r, x + r, y + r), # bounding box (x0, y0, x1, y1)
+            fill=color, outline=RED
+        )
+
+        # recurse fractally by threes
+        # circles
+
+
 
 def main(filename_base):
     images = []
@@ -39,7 +58,11 @@ def main(filename_base):
 
         im = Image.new('RGB', (W, W), BLACK)
         d = ImageDraw.Draw(im)
-        circles(d, scale)
+
+        # the first branch
+        circles(d, 3, 100 * scale, 100 * scale, 100 * scale)
+
+        # notations in the corner
         d.multiline_text((10,10), f"{step_cnt}", font=font, fill=WHITE)
 
         images.append(im)
